098fb4ea5af71f11676060aa637a0fa69028bbb0
Tru Nguyen <trunguyen@Trus-MacBook-Pro.local>
2019-02-11 17:09:50 -0600

    creation of meal_screen

---
+++ app/src/main/java/com/example/stackrecette/MealScreen.java
@@ -0,0 +1,13 @@
+package com.example.stackrecette;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MealScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_screen);
+    }
+}
