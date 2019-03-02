0ac2c8731772acf98ff898decd05487956e04ef5
quoctru99 <nguyenquoctru99@yahoo.com>
2019-03-01 22:35:42 -0600

    finsihed meal_screen with search engine

--- app/src/main/java/com/example/stackrecette/MealScreen.java
+++ app/src/main/java/com/example/stackrecette/MealScreen.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +38,7 @@ public class MealScreen extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
 
+    private static final String url = "http://www.recipepuppy.com/api/";
     private TextView food_name;
     private ImageView food_image;
     private SearchView search;
@@ -69,12 +71,22 @@ public class MealScreen extends AppCompatActivity {
                 mAuth.signOut();
             }
         });
-
-        food_name = findViewById(R.id.food_name1);
-        food_image = findViewById(R.id.imageView1);
+        
         search = findViewById(R.id.search_input);
 
-        
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                new choose_meal().execute(url+"?i="+query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
     }
 
 
@@ -121,27 +133,27 @@ public class MealScreen extends AppCompatActivity {
         {
             super.onPostExecute(result);
             Object[][] data = handle_json(result.toString());
-            food_name.setText(data[0][0].toString());
-            Picasso.get().load(data[0][data[0].length-1].toString()).into(food_image);
+            setText(data);
             System.out.println("onpost" + result);
         }
 
         //this method will use to handle json
         public Object[][] handle_json (String res)
         {
-            int num_result = 6; //default
             JSONArray arr = null;
             //data of var_data will be { {food_name,ingredient,img} , {food_name,ingredient,img} }
-            Object[][] data = new Object[num_result][3];
+            Object[][] data = null;
             try {
                 arr = new JSONObject(res).getJSONArray("results");
+                data = new Object[arr.length()][3];
+                System.out.println(arr.length());
             } catch (Exception e) {
                 System.out.println("no element results found");
             }
 
             try {
                 System.out.println("handling json...");
-                for(int i=0;i< num_result; i++)
+                for(int i=0;i< arr.length(); i++)
                 {
                     JSONObject res_obj = arr.getJSONObject(i);
 
@@ -153,9 +165,26 @@ public class MealScreen extends AppCompatActivity {
             } catch (Exception e) {
                 System.out.println(" unable to loop throught results obj json");
             }
-
-            System.out.println("in handle json"  + Arrays.deepToString((data)));
             return data;
+        }
+
+        public void setText (Object[][] data)
+        {
+            int[] arr_foodname = {R.id.food_name1,R.id.food_name2,R.id.food_name3,R.id.food_name4,R.id.food_name5,R.id.food_name6,R.id.food_name7};
+            int[] arr_foodimage = {R.id.imageView1,R.id.imageView2,R.id.imageView3,R.id.imageView4,R.id.imageView5,R.id.imageView6,R.id.imageView7};
+            int[] arr_ll = {R.id.ll1,R.id.ll2,R.id.ll3,R.id.ll4,R.id.ll5,R.id.ll6,R.id.ll7};
+            System.out.println(Arrays.deepToString(data));
+            for(int i=0;i<arr_foodname.length;i++)
+            {
+                food_name = findViewById(arr_foodname[i]);
+                food_image = findViewById(arr_foodimage[i]);
+                food_name.setText(data[i][0].toString());
+
+                if(!String.valueOf(data[i][data[0].length-1]).isEmpty()) {
+                    Picasso.get().load(data[i][data[0].length - 1].toString()).into(food_image);
+                }
+                findViewById(arr_ll[i]).setVisibility(View.VISIBLE);
+            }
         }
     }
 
