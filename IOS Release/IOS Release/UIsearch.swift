8ab80106da483e6afbbb5d8142a30730ab52a532
Tru Nguyen <trunguyen@Trus-MBP.attlocal.net>
2019-04-23 11:51:05 -0500

    create IOS Release

---
+++ IOS Release/IOS Release/UIsearch.swift
@@ -0,0 +1,17 @@
+//
+//  UIsearch.swift
+//  IOS Release
+//
+//  Created by Tru Nguyen on 4/23/19.
+//  Copyright © 2019 Tru Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class UISearchBar : UIView
+{
+    func viewDidLoad()
+    {
+        print("as")
+    }
+}
---
+++ IOS Release/IOS Release/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS Release
+//
+//  Created by Tru Nguyen on 4/23/19.
+//  Copyright © 2019 Tru Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
