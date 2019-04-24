14ca6cb2fc125c445aafe667b7d5237a46031e02
Tru Nguyen <trunguyen@Trus-MBP.attlocal.net>
2019-04-23 23:28:36 -0500

    create Table view

--- IOS Release/IOS Release/ViewController.swift
+++ IOS Release/IOS Release/ViewController.swift
@@ -8,11 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    var foodArr = [food]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpFood()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableCell else {
+            return UITableViewCell()
+        }
+        
+        if let data: Data = getData(img: foodArr[indexPath.row].img) {
+            cell.food_name.text = foodArr[indexPath.row].food_name
+            cell.img.image = UIImage(data: data)
+        } else {
+            cell.food_name.text = foodArr[indexPath.row].food_name
+            cell.img.image = UIImage(named: foodArr[indexPath.row].img)
+        }
+        
+        
+        return cell
+    }
+    
+    func setUpFood()
+    {
+        foodArr.append(food(food_name: "cc", img:"http://img.recipepuppy.com/514820.jpg"))
+        foodArr.append(food(food_name: "bs", img:"2"))
+        foodArr.append(food(food_name: "ccdd", img:"http://img.recipepuppy.com/514820.jpg"))
+        foodArr.append(food(food_name: "ccw", img:"http://img.recipepuppy.com/514820.jpg"))
+    }
+    
+    func getData (img: String) -> Data?
+    {
+        if let url = URL(string: img) {
+            do {
+                let data = try Data(contentsOf: url)
+                return data
+            } catch let err {
+                print("error : \(err.localizedDescription)")
+            }
+        }
+        return nil
     }
     
     
---
+++ IOS Release/IOS Release/food.swift
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+import UIKit
+
+class food {
+    let food_name: String
+    let img: String
+    
+    init(food_name: String, img: String)
+    {
+        self.food_name = food_name
+        self.img = img
+    }
+}
